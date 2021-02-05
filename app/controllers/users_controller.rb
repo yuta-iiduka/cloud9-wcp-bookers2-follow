b85f25662c3e59ca73d79b274849be8ccde00158
yuta-iiduka <ota12041204@gmail.com>
2021-02-05 12:52:24 +0000

    課題４

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def following
+    @title = "Follow Users"
+    @user = User.find(params[:id])
+    @users = @user.following#.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Follower Users"
+    @user = User.find(params[:id])
+    @users =@user.followers#.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+	#Relationshipクラスのfollower_idから関連付ける。
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+	#followedsという名前をfollowingに変更
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
+  attachment :profile_image, destroy: false
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+
+  def feed
+
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  private
+
+end
